62d26491d94b0e18b90531b22bfbca85e8acc675
Denis <mistrny@gmail.com>
2023-04-05 22:58:32 +0300

    task2

--- solution.py
+++ solution.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import chi2
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 264956206  # Ваш chat ID, не меняйте название переменной
+
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    s_squared = np.sum((x - np.mean(x)) ** 2)
+    left_border = np.sqrt(((s_squared) / (17 * chi2.ppf(1 - alpha / 2, len(x) - 1))))
+    right_border = np.sqrt(((s_squared) / (17 * chi2.ppf(alpha / 2, len(x) - 1))))
+    if left_border > right_border:
+        return right_border, left_border
+    return left_border, right_border
