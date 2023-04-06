8b40f98c7b080c786df19f6ccd868f0fca248486
Denis <mistrny@gmail.com>
2023-04-06 11:43:24 +0300

    attempt2

--- solution.py
+++ solution.py
@@ -11,10 +11,11 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    scale = 17
     alpha = 1 - p
-    s_squared = np.sum((x - np.mean(x)) ** 2)
-    left_border = np.sqrt(((s_squared) / (17 * chi2.ppf(1 - alpha / 2, len(x) - 1))))
-    right_border = np.sqrt(((s_squared) / (17 * chi2.ppf(alpha / 2, len(x) - 1))))
-    if left_border > right_border:
-        return right_border, left_border
+    s_squared = np.sum(x**2)
+    z1 = chi2.ppf(1 - alpha / 2, df=len(x) * 2)
+    z2 = chi2.ppf(alpha / 2, df=len(x) * 2)
+    left_border = np.sqrt(s_squared / (z1 * scale))
+    right_border = np.sqrt(s_squared / (z2 * scale))
     return left_border, right_border
